10a234f851f14230c2fd5a29ca3978d4cdb78e1e
Marcella Pacobahyba <marcellapacobahyba@gmail.com>
2025-10-03 21:27:56 +0100

    modificações 10

--- graphql/app.js
+++ graphql/app.js
@@ -16,10 +16,6 @@ const server = new ApolloServer({
 async function startApolloServer() {
   await server.start();
   server.applyMiddleware({ app });
-  const PORT = process.env.PORT || 4000;
-  app.listen(PORT, () => {
-    console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`);
-  });
 }
 
 startApolloServer();
