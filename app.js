4c476970a5672e473bf5ee624a00b89dc07ac090
Marcella Pacobahyba <marcellapacobahyba@gmail.com>
2025-10-09 23:32:53 +0100

    mod app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const resolvers = require('./graphql/resolvers');
 const authenticate = require('./graphql/authenticate');
 
 
-const transferController = require('./rest/controllers/transferController');
+const transferController = require('./rest/controllers/transferController.test.js');
 const authController = require('./rest/controllers/authController');
 
 const app = express();
